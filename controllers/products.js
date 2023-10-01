13c67dfcb5814d3fd30097c83a0cebc4cbddca17
Talat09 <talat181008312@gmail.com>
2023-10-02 03:17:07 +0600

    done sort functionality in api

--- controllers/products.js
+++ controllers/products.js
@@ -1,6 +1,6 @@
 const Product = require("../models/product");
 const getAllProducts = async (req, res) => {
-  const { company, name, featured } = req.query;
+  const { company, name, featured, sort } = req.query;
   const queryObject = {};
   if (company) {
     queryObject.company = company;
@@ -11,13 +11,17 @@ const getAllProducts = async (req, res) => {
   if (name) {
     queryObject.name = { $regex: name, $options: "i" };
   }
-
+  let result = Product.find(queryObject);
+  if (sort) {
+    let sortFix = sort.replace(",", " ");
+    result = result.sort(sortFix);
+  }
   console.log(queryObject);
-  const productsData = await Product.find(queryObject);
+  const productsData = await result;
   res.status(200).json({ productsData });
 };
 const getAllProductsTesting = async (req, res) => {
-  const productsData = await Product.find({});
+  const productsData = await Product.find(req.query).sort("-name price");
 
   res.status(200).json({ productsData });
 };
