ac5acfa62ac6e94c31eb52148ef082e5d65c0a0d
Talat09 <talat181008312@gmail.com>
2023-10-02 02:19:59 +0600

    done search query by company name and product name

--- controllers/products.js
+++ controllers/products.js
@@ -1,10 +1,11 @@
 const Product = require("../models/product");
 const getAllProducts = async (req, res) => {
-  const productsData = await Product.find({});
+  const productsData = await Product.find(req.query);
   res.status(200).json({ productsData });
 };
 const getAllProductsTesting = async (req, res) => {
-  const productsData = await Product.find({ company: "oneplus" });
+  const productsData = await Product.find({});
+
   res.status(200).json({ productsData });
 };
 module.exports = { getAllProducts, getAllProductsTesting };
