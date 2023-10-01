b153cb8ca949e5652356bac90922aaad8d31cf05
Talat09 <talat181008312@gmail.com>
2023-10-02 02:48:33 +0600

    done search functionality in api

--- controllers/products.js
+++ controllers/products.js
@@ -1,11 +1,18 @@
 const Product = require("../models/product");
 const getAllProducts = async (req, res) => {
-  const { company } = req.query;
+  const { company, name, featured } = req.query;
   const queryObject = {};
   if (company) {
     queryObject.company = company;
-    console.log(queryObject);
   }
+  if (featured) {
+    queryObject.featured = featured;
+  }
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
+
+  console.log(queryObject);
   const productsData = await Product.find(queryObject);
   res.status(200).json({ productsData });
 };
